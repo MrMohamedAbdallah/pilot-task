cf2892e8bb0d9ba054902f357292abef1123db8b
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2021-05-08 09:37:31 +0200

    Install authentication features
    Added authentication features using laravel/breez
    Added required columns to user table
    Change default string length to 191 on AppServiceProvider boot method

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('job_title')->nullable();
+            $table->string('profile_pic')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
